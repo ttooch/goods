a1c991817f15957bd77d2aeac83e520495ac54d0
yanxin <546224964@qq.com>
2018-03-24 15:45:17 +0800

    骚操作

--- handler/base.go
+++ handler/base.go
@@ -2,14 +2,11 @@ package handler
 
 import (
 	"encoding/json"
-	"fmt"
 )
 
 func DeepCopy(model interface{}, rpc interface{}) {
 
 	b, _ := json.Marshal(model)
-
-	fmt.Println(string(b))
 
 	_ = json.Unmarshal(b, rpc)
 
--- handler/goods.go
+++ handler/goods.go
@@ -9,61 +9,22 @@ import (
 	goods "github.com/ttooch/proto/goods"
 	//"github.com/ttooch/goods/model"
 	//"os/user"
-	//"github.com/ttooch/goods/model"
+	"github.com/ttooch/goods/model"
 )
 
 type Goods struct{}
 
 func (e *Goods) Detail(ctx context.Context, req *goods.Request, rsp *goods.Response) error {
-	//log.Log("Received Goods.Detail request")
-	//ads := model.GetAdsForPage(1, 20)
 
-	//rpcAds := make([]*goods.Ads, 0, 20)
+	rpcGoods := make([]*goods.Good, 0)
 
-	//DeepCopy(ads, &rpcAds)
+	ads := model.GetGoodsForPage(1, 3)
 
-	//rsp.Data = rpcAds
+	DeepCopy(ads, &rpcGoods)
 
-	rsp.Msg = "goodsDetail"
+	rsp.Data = rpcGoods
 
-	//personDB := make(map[string]goods.Response)
-
-	//var person = goods.Response{123, "msg", "bbb"}
-	//ads := goods.Ads{1,"2","2","2","2",3,3}
-	ad := goods.Ads{1,"2","2","2","2",3,3}
-
-	rsp.Status = 1
-
-	ads  := make([]*goods.Ads, 0)
-
-	ads = append(ads, &ad)
-
-	fmt.Println(ad)
-
-	fmt.Println(&ad)
-
-	fmt.Println(*&ad)
-
-	rsp.Data = ads
-
-	//fmt.Println("111")
-	//
-	//go service.PubGoods()
-	//
-	//fmt.Println("PubGoods1")
-	//
-	//go service.PubGoods()
-	//
-	//fmt.Println("PubGoods2")
-
-	//var m1 map[string]int32
-	//
-	//m1 = make(map[string]int32)
-	//
-	//m1["a"] = 12312
-	//m1["b"] = 565464
-
-	fmt.Println(ads)
+	fmt.Println(ads[0])
 
 	return nil
 }
